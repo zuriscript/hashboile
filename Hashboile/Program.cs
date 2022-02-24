edd3d4ea632675c96550fa335a4411025b61e37b
Dana Shmaria <dana.shmaria@swisscom.com>
2022-02-24 19:12:51 +0100

    Add input files

--- Hashboile/Program.cs
+++ Hashboile/Program.cs
@@ -9,25 +9,29 @@ runner.Run(
     MetricDirective.General);
 
 runner.RunWithSimulation(
-    "b_basic.in.txt", 
-    "b_basic.out.txt", 
+    "b_better_start_small.in.in.txt", 
+    "b_better_start_small.in.out.txt", 
     MetricDirective.General, 
     1);
 
 runner.RunWithSimulation(
-    "c_coarse.in.txt",
-    "c_coarse.out.txt",
+    "c_collaboration.in.in.txt",
+    "c_collaboration.in.out.txt",
     MetricDirective.General,
     4);
 
 runner.RunWithSimulation(
-    "d_difficult.in.txt",
-    "d_difficult.out.txt",
+    "d_dense_schedule.in.in.txt",
+    "d_dense_schedule.in.out.txt",
     MetricDirective.General,
     20);
 
 runner.Run(
-    "e_elaborate.in.txt",
-    "e_elaborate.out.txt",
+    "e_exceptional_skills.in.in.txt",
+    "e_exceptional_skills.in.out.txt",
     MetricDirective.General);
 
+runner.Run(
+    "f_find_great_mentors.in.in.in.txt",
+    "f_find_great_mentors.in.in.out.txt",
+    MetricDirective.General);
